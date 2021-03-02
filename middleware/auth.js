a0a2376dff23aaf7813fb33841efa81d80aa89ea
DHaligowski <dhaligowski@gmail.com>
2021-03-01 23:33:19 -0500

    Refactor middleware components.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,17 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
-function auth (req, res, next){
+function auth(req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token) return res.status(401).send("Access denied.  No token provided");
 
-    const token = req.header('x-auth-token');
-    if(!token) return res.status(401).send('Access denied.  No token provided');
-    
-    try {
-    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+  try {
+    const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
     req.user = decoded;
     next();
-   } 
-   catch (error) {
-       res.status(400).send('Invalid token.');
-   }
- }
+  } catch (error) {
+    res.status(400).send("Invalid token.");
+  }
+}
 
 module.exports = auth;
--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,9 @@
- const winston = require('winston');
- 
- function error(err, req, res, next){
-    winston.error(err.message, err);
-    
-    res.status(500).send('Something failed'); 
+const winston = require("winston");
+
+function error(err, req, res, next) {
+  winston.error(err.message, err);
+
+  res.status(500).send("Something failed");
 }
 
 module.exports = error;
