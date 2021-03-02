42d7d4a9c3062f87d064dff9e07ed4859e46ba0a
DHaligowski <dhaligowski@gmail.com>
2021-03-01 23:37:42 -0500

    Refactor tickets components.

--- tickets/tickets.js
+++ tickets/tickets.js
@@ -1,9 +1,11 @@
 const tickets = [
-    { id: 1, title: "Sample Ticket", value: "Ticket 1", description: "Description Ticket 1", status: "Open" },
-    //{ id: 2, title: "Apt 2", value: "Ticket 2", description: "Bathroom", status: "Closed" },
-    //{ id: 3, title: "Apt 3", value: "Ticket 3", description: "Water", status: "Open" }
-  ];
-  
+  {
+    id: 1,
+    title: "Sample Ticket",
+    value: "Ticket 1",
+    description: "Description Ticket 1",
+    status: "Open",
+  },
+];
 
 module.exports = tickets;
-  
